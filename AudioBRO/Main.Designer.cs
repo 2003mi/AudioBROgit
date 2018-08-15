745a4030fbdd1f69bb2827d640d0b46af2b8a173
Mikal <38553900+2003mi@users.noreply.github.com>
2018-08-15 11:43:31 +0200

    This is AudioBRO
    I dont know if this matter but maby you must do sothing whit NAudio.dll if its not working

---
+++ AudioBRO/Main.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace AudioBRO
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.divicelist = new System.Windows.Forms.ComboBox();
+            this.tickq = new System.Windows.Forms.Timer(this.components);
+            this.coun = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // divicelist
+            // 
+            this.divicelist.FormattingEnabled = true;
+            this.divicelist.Location = new System.Drawing.Point(12, 9);
+            this.divicelist.Name = "divicelist";
+            this.divicelist.Size = new System.Drawing.Size(714, 21);
+            this.divicelist.TabIndex = 2;
+            // 
+            // tickq
+            // 
+            this.tickq.Enabled = true;
+            this.tickq.Interval = 10;
+            this.tickq.Tick += new System.EventHandler(this.tickq_Tick);
+            // 
+            // coun
+            // 
+            this.coun.AutoSize = true;
+            this.coun.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.coun.Location = new System.Drawing.Point(725, 33);
+            this.coun.Name = "coun";
+            this.coun.Size = new System.Drawing.Size(59, 31);
+            this.coun.TabIndex = 3;
+            this.coun.Text = "100";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(714, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "                                                                                 " +
+    "                   ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(775, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "50";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(754, 0);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(778, 76);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.coun);
+            this.Controls.Add(this.divicelist);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(794, 115);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Audio BRO";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox divicelist;
+        private System.Windows.Forms.Timer tickq;
+        private System.Windows.Forms.Label coun;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ AudioBRO/Main.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+
+namespace AudioBRO
+{
+    public partial class Main : Form
+    {
+
+        int vol;
+        string vlogs;
+
+        public Main()
+        {
+            InitializeComponent();
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            var devices = enumerator.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active);
+            divicelist.Items.AddRange(devices.ToArray());
+            label1.Text = null;
+            label2.Text = null;
+            coun.Text = null;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            try
+            {
+                pictureBox1.Image = Image.FromFile(@".\AudioMEME\frame_00_delay-0.1s.gif");
+                pictureBox1.Image = null;
+                this.Size = new Size(816, 860);
+            }
+            catch
+            {
+                pictureBox1.Image = null;
+            }
+        }
+
+        private void tickq_Tick(object sender, EventArgs e)
+        {
+            if(divicelist.SelectedItem != null)
+            {
+                var device = (MMDevice)divicelist.SelectedItem;
+                vol = (int)(Math.Round(device.AudioMeterInformation.MasterPeakValue * 100));
+                coun.Text = vol.ToString();
+
+                string tabs = new String(' ', vol);
+                label1.Text = tabs;
+
+
+                
+                int volg = vol / 2;
+                if(volg < 10)
+                {
+                    vlogs = @".\AudioMEME\frame_0" + volg + "_delay-0.1s.gif";
+                }
+                else
+                {
+                    vlogs = @".\AudioMEME\frame_" + volg + "_delay-0.1s.gif";
+                }
+                try
+                {
+                    pictureBox1.Image = Image.FromFile(vlogs);
+                    label2.Text = (vol / 2).ToString();
+                }
+                catch
+                {
+                    pictureBox1.Image = null;
+                    label2.Text = null;
+                }
+            }
+        }
+    }
+}
